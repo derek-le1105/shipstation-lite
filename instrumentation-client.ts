088493b3ab47a1d557c43cdece4e7b25e7bf1375
Derek Le <derekle1105@gmail.com>
2025-12-18 11:24:02 -0800

    testing reverse proxy setup

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,6 +2,7 @@
 import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  api_host: "/ph",
+  ui_host: "https://us.posthog.com",
   defaults: "2025-11-30",
 });
