24382e3accbff42fb0c9843f288a63604988439c
Derek Le <derekle1105@gmail.com>
2026-01-02 10:27:57 -0800

    refactored date filter popover.
    refined ui to explicity show user which dates are being set when selecting pre-defined settings

--- components/labels/label-date-popover.tsx
+++ components/labels/label-date-popover.tsx
@@ -17,9 +17,18 @@ type RadioOptions =
   | "custom";
 
 export default function LabelDatePopover<T>({ table }: { table: Table<T> }) {
-  const [startDate, setStartDate] = useState<Date | undefined>(undefined);
-  const [endDate, setEndDate] = useState<Date | undefined>(undefined);
-  const [selected, setSelected] = useState<RadioOptions | undefined>(undefined);
+  const [open, setOpen] = useState<boolean>(false);
+  const [selected, setSelected] = useState<RadioOptions | undefined | null>(
+    undefined
+  );
+
+  const [currentFilter, setCurrentFilter] = useState<{
+    from: Date | undefined;
+    to: Date | undefined;
+  }>({
+    from: undefined,
+    to: undefined,
+  });
 
   const createdAtFilter = table.getColumn("created_at");
 
@@ -47,31 +56,38 @@ export default function LabelDatePopover<T>({ table }: { table: Table<T> }) {
       default:
         break;
     }
-    if (value !== "custom") createdAtFilter?.setFilterValue({ from, to });
-  };
-
-  const handleStartDateSet = (newDate: Date) => {
-    setStartDate(newDate);
-    createdAtFilter?.setFilterValue({ from: newDate, to: endDate });
-  };
-
-  const handleEndDateSet = (newDate: Date) => {
-    setEndDate(newDate);
-    createdAtFilter?.setFilterValue({ from: startDate, to: newDate });
+    if (value !== "custom") setCurrentFilter({ from, to });
   };
 
   const handleStartDateClear = () => {
-    setStartDate(undefined);
-    createdAtFilter?.setFilterValue({ from: undefined, to: endDate });
+    setCurrentFilter({ ...currentFilter, from: undefined });
   };
 
   const handleEndDateClear = () => {
-    setEndDate(undefined);
-    createdAtFilter?.setFilterValue({ from: startDate, to: undefined });
+    setCurrentFilter({ ...currentFilter, to: undefined });
+  };
+
+  const handleDateRangeSet = ({ from, to }: { from?: Date; to?: Date }) => {
+    const nextFilter = { ...currentFilter };
+    if (from != null) nextFilter.from = from;
+    if (to != null) nextFilter.to = to;
+    setCurrentFilter(nextFilter);
+  };
+
+  const handleDateRangeClear = () => {
+    setSelected(null);
+    setCurrentFilter({ from: undefined, to: undefined });
+    createdAtFilter?.setFilterValue(null);
+    setOpen(false);
+  };
+
+  const handleDateRangeApply = () => {
+    createdAtFilter?.setFilterValue({ ...currentFilter });
+    setOpen(false);
   };
 
   return (
-    <Popover>
+    <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <div className="relative">
           <div className="absolute -top-2 -right-1">
@@ -88,64 +104,102 @@ export default function LabelDatePopover<T>({ table }: { table: Table<T> }) {
           </Button>
         </div>
       </PopoverTrigger>
-      <PopoverContent align="end" className="w-full">
-        <div className="flex flex-col gap-4">
-          <RadioGroup value={selected} onValueChange={handleSelectedChange}>
-            <div className="flex items-center gap-3">
-              <RadioGroupItem value="today" id="today" />
-              <Label htmlFor="today">Today</Label>
-            </div>
-            <div className="flex items-center gap-3">
-              <RadioGroupItem value="last7" id="last7" />
-              <Label htmlFor="last7">Last 7 days</Label>
-            </div>
-            <div className="flex items-center gap-3">
-              <RadioGroupItem value="last30" id="last30" />
-              <Label htmlFor="last30">Last 30 days</Label>
-            </div>
-            <div className="flex items-center gap-3">
-              <RadioGroupItem value="last90" id="last90" />
-              <Label htmlFor="last90">Last 90 days</Label>
-            </div>
-            <div className="flex items-center gap-3">
-              <RadioGroupItem value="last12m" id="last12m" />
-              <Label htmlFor="last12m">Last 12 months</Label>
-            </div>
-            <div className="flex items-center gap-3">
-              <RadioGroupItem value="custom" id="custom" />
-              <Label htmlFor="custom">Custom</Label>
-            </div>
-          </RadioGroup>
-          {selected === "custom" && (
-            <div className="flex flex-col gap-2">
-              <div className="flex flex-col items-start gap-2">
-                <div className="flex items-center justify-between w-full">
-                  <Label>Starting</Label>
-                  <Button
-                    variant="link"
-                    className="p-0 h-full"
-                    onClick={handleStartDateClear}
-                  >
-                    Clear
-                  </Button>
-                </div>
-                <DatePicker date={startDate} setDate={handleStartDateSet} />
+      <PopoverContent align="end" className="w-full p-0">
+        <div>
+          <div className="grid grid-cols-[220px_1fr]">
+            <div className="flex flex-col">
+              <div className="px-5 py-4 text-lg font-semibold border-b border-r">
+                Date Range
               </div>
-              <div className="flex flex-col items-start gap-2">
-                <div className="flex items-center justify-between w-full">
-                  <Label>Ending</Label>
-                  <Button
-                    variant="link"
-                    className="p-0 h-full"
-                    onClick={handleEndDateClear}
-                  >
-                    Clear
-                  </Button>
-                </div>
-                <DatePicker date={endDate} setDate={handleEndDateSet} />
+              <div className="border-r border-border px-4 py-6">
+                <RadioGroup
+                  value={selected}
+                  onValueChange={handleSelectedChange}
+                >
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="today" id="today" />
+                    <Label htmlFor="today">Today</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="last7" id="last7" />
+                    <Label htmlFor="last7">Last 7 days</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="last30" id="last30" />
+                    <Label htmlFor="last30">Last 30 days</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="last90" id="last90" />
+                    <Label htmlFor="last90">Last 90 days</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="last12m" id="last12m" />
+                    <Label htmlFor="last12m">Last 12 months</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="custom" id="custom" />
+                    <Label htmlFor="custom">Custom</Label>
+                  </div>
+                </RadioGroup>
               </div>
             </div>
-          )}
+            <div className="border-r border-border px-4 py-6">
+              <div className="flex flex-col gap-10 py-4">
+                <div className="flex flex-col items-start gap-2">
+                  <div className="flex items-center justify-between w-full">
+                    <Label>Starting</Label>
+                    <Button
+                      variant="link"
+                      className="p-0 h-full"
+                      onClick={handleStartDateClear}
+                    >
+                      Clear
+                    </Button>
+                  </div>
+                  <DatePicker
+                    date={currentFilter.from}
+                    setDate={(newDate) => {
+                      handleDateRangeSet({ from: newDate });
+                    }}
+                  />
+                </div>
+                <div className="flex flex-col items-start gap-2">
+                  <div className="flex items-center justify-between w-full">
+                    <Label>Ending</Label>
+                    <Button
+                      variant="link"
+                      className="p-0 h-full"
+                      onClick={handleEndDateClear}
+                    >
+                      Clear
+                    </Button>
+                  </div>
+                  <DatePicker
+                    date={currentFilter.to}
+                    setDate={(newDate) => {
+                      handleDateRangeSet({ to: newDate });
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between items-center border-t px-5 py-4">
+            <Button variant="outline" onClick={handleDateRangeClear}>
+              Clear
+            </Button>
+            <div className="flex justify-between items-center gap-2">
+              <Button
+                variant="outline"
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button onClick={handleDateRangeApply}>Apply</Button>
+            </div>
+          </div>
         </div>
       </PopoverContent>
     </Popover>
